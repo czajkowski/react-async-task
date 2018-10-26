adb23452304004ae1e9aa80ec80a62f8bb81324d
Piotr Czajkowski <piotr.czajkowski@blebox.eu>
2018-10-26 07:49:30 +0200

    Component implementation.

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,64 @@ import PropTypes from 'prop-types';
 const isFunction = fn => typeof fn === 'function';
 const { Consumer, Provider } = React.createContext();
 
+const STATUS = {
+    Loading: 'loading',
+    Resolved: 'resolved',
+    Rejected: 'rejected',
+};
+
 const createTask = (task) => {
+    let id = 0;
+
     class AsyncTask extends Component {
         static propTypes = {
             children: PropTypes.node,
         };
+
+        mounted = false;
+
+        state = {
+            data: null,
+            error: null,
+            status: STATUS.Loading,
+        };
+
+        componentDidMount() {
+            this.mounted = true;
+            this.runTask();
+        }
+
+        componentWillUnmount() {
+            this.mounted = false;
+        }
+
+        onResolved = taskId => (data) => {
+            if (this.mounted && id === taskId) {
+                this.setState({
+                    data,
+                    status: STATUS.Resolved,
+                });
+            }
+        }
+
+        onRejected = taskId => (error) => {
+            if (this.mounted && id === taskId) {
+                this.setState({
+                    error,
+                    status: STATUS.Rejected,
+                });
+            }
+        }
+
+        runTask() {
+            id += 1;
+
+            Promise
+                .resolve()
+                .then(() => task(this.props))
+                .then(this.onResolve(id))
+                .catch(this.onReject(id));
+        }
 
         render() {
             const { children } = this.props;
@@ -25,10 +78,48 @@ const createTask = (task) => {
         }
     }
 
-    AsyncTask.Pending = ({ children }) => {};
-    AsyncTask.Loading = ({ children }) => {};
-    AsyncTask.Resolved = ({ children }) => {};
-    AsyncTask.Rejected = ({ children }) => {};
+    AsyncTask.Loading = ({ children }) => (
+        <Consumer>
+            {({ status }) => {
+                if (status === STATUS.Loading) {
+                    return isFunction(children) ? children() : children;
+                }
+                return null;
+            }}
+        </Consumer>
+    );
+    AsyncTask.Loading.propTypes = {
+        children: PropTypes.node,
+    };
+
+
+    AsyncTask.Resolved = ({ children }) => (
+        <Consumer>
+            {({ status, data }) => {
+                if (status === STATUS.Resolved) {
+                    return isFunction(children) ? children(data) : children;
+                }
+                return null;
+            }}
+        </Consumer>
+    );
+    AsyncTask.Resolved.propTypes = {
+        children: PropTypes.node,
+    };
+
+    AsyncTask.Rejected = ({ children }) => (
+        <Consumer>
+            {({ status, error }) => {
+                if (status === STATUS.Rejected) {
+                    return isFunction(children) ? children(error) : children;
+                }
+                return null;
+            }}
+        </Consumer>
+    );
+    AsyncTask.Rejected.propTypes = {
+        children: PropTypes.node,
+    };
 
     return AsyncTask;
 };
